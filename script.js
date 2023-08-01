66bb49fdd5ac8ce5cb44d34182c763bba31f347e
maxisnotarichguy@gmail.com <65668267+Hostman123456@users.noreply.github.com>
2023-08-01 08:48:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -10,16 +10,25 @@ let color_display = document.getElementById("color-display")
 
 aqua.onclick = function() {
  color_display.style.backgroundColor = "aqua"
+ color_display.innerHTML = "aqua"
 }
 teal.onclick = function() {
     color_display.style.backgroundColor = "teal"
-   }
+    color_display.innerHTML = "teal"
+}
+   
 pink.onclick = function() {
     color_display.style.backgroundColor = "pink"
-   }
+    color_display.innerHTML = "pink"
+}
+   
 purple.onclick = function() {
     color_display.style.backgroundColor = "purple"
-   }
+    color_display.innerHTML = "purple"
+}
+   
 colorless.onclick = function() {
     color_display.style.backgroundColor = "white"
-   }
+    color_display.innerHTML = "colorless"
+}
+   
